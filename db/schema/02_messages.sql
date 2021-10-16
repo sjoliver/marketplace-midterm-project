fb8bda7325941309b541e48a096d96d3d6d68c17
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2021-10-16 10:36:02 -0700

    edit schema

--- db/schema/02_messages.sql
+++ db/schema/02_messages.sql
@@ -2,9 +2,9 @@ DROP TABLE IF EXISTS messages CASCADE;
 
 CREATE TABLE messages (
   id SERIAL PRIMARY KEY NOT NULL,
-  buyer_id INTEGER REFERENCES users(id),
-  seller_id INTEGER REFERENCES users(id),
-  listing_id INTEGER REFERENCES users(id),
+  buyer_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  seller_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  listing_id INTEGER REFERENCES listings(id) ON DELETE CASCADE,
   subject VARCHAR(255) NOT NULL,
   message TEXT
 );
