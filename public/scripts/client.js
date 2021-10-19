87b97a22ef196cdcb8ba42121104b85f60865899
Sophie <sophiejoliver19+1@gmail.com>
2021-10-19 15:58:29 +0000

    add logic for messaging ajax requests

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,5 +1,4 @@
-// Client facing scripts here
-
+//-> // Client facing scripts here
 
 document.body.addEventListener('click', updatePage);
 
@@ -51,3 +50,74 @@ function loadWatching() {
   });
 }
 
+// // MESSAGING LOGIC
+
+// $(document).ready(function () {
+
+//   $("form.response-form").on("submit", function(event) {
+//     event.preventDefualt();
+
+//     const data = $(this).serialize();
+
+//     // error handling
+//     if (data === null || data === "text=") {
+//       errorMessage = "Message cannot be empty, please enter text before sending.";
+//       $('.error-message').text(errorMessage);
+//       // $('#error-container').slideDown(400);
+//       return;
+//     }
+
+//     console.log("HI");
+//     // // POST REQUEST: processing form submission
+//     // $.ajax({
+//     //   url: '/messages/reply', // REFACTOR -- how do I get the message id param here??
+//     //   method: 'POST',
+//     //   contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+//     //   dataType: 'json',
+//     //   // data: JSON.stringify({ message: $(this).serialize(), subject: $('header h4').text() }),
+//     //   // data: $(this).serialize(),
+//     // }).then((result) => {
+//     //   loadMessages();
+//     // }).catch((error) => {
+//     //   console.log(`there was an error: ${error}`);
+//     // })
+
+//     // // clears textarea once user submits new tweet
+//     // $('.reply-message-body').val('');
+//   });
+
+//   const loadMessages = function () {
+
+//     $.ajax({
+//       url:'/messages/reply', // REFACTOR -- how do I get the message id param here??
+//       method: 'GET'
+//     }).then(data => {
+//       console.log(data)
+//       // createMessageElement(data)
+//     })
+
+//   }
+
+
+//   // CREATE MESSAGE
+//   const createMessageElement = function (messageObject) {
+
+//     const escape = function (str) {
+//       let div = document.createElement("div");
+//       div.appendChild(document.createTextNode(str));
+//       return div.innerHTML;
+//     };
+
+//     const createdTime = timeago.format(messageObject["created_at"])
+
+//     const $message = `
+//       <div class="container">
+//         <span class="sender">${messageObject.sender}</span>
+//         <p>${messageObject.message}</p>
+//         <span class="time-right">${createdTime}/span>
+//       </div>
+//     `
+//     return $message;
+//   }
+
+// });
